d2b33dc91c992603900c94e7ff8639661a2d85aa
rafaelfantini1 <50959639+rafaelfantini1@users.noreply.github.com>
2020-08-07 21:27:42 -0300

    continuacao do codigo VotaJava

--- VotaJava.java
+++ VotaJava.java
@@ -1,5 +1,15 @@
+import java.util.Scanner;
+
 public class VotaJava{
-    public static void main(){
+    public static void main(String[] args){
+    	Scanner scanner = new Scanner(System.in);
         System.out.println("Quantos anos voce tem?");
+        int idade = scanner.nextInt();
+        if(idade >= 18)	{
+        	System.out.println("Voto Obrigatorio");
+        } else {
+        	System.out.println("Voto Nao Obrigatorio");
+        }	
+      
     }
 }
